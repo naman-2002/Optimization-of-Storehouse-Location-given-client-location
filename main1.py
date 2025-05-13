e1edf8d8166602d56bb52b8640c9db1cb4aaf785
Naman Joshi <77952562+naman-2002@users.noreply.github.com>
2025-05-13 13:11:35 +0530

    Update main1.py

--- main1.py
+++ main1.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}}) 
 
 points = []  # Initialize an empty list to store points
 
